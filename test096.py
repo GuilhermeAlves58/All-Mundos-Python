2c8ec21788bc7c1489ff5584804ae6ce03767210
perkles <guilhermealves5194@gmail.com>
2025-03-31 18:03:00 -0300

    daily study

---
+++ test096.py
@@ -0,0 +1,7 @@
+def area(width,length):
+    a = length * width
+    print(f"area is {a}m²")
+
+lenght = float(input('Type the lenght: '))
+width = float(input('Type the width: '))
+area(lenght,width)
