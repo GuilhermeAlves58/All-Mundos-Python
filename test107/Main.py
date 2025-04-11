6267600585e7d0475b508de55bbf145f07b00c57
perkles <guilhermealves5194@gmail.com>
2025-04-11 16:16:13 -0300

    daily study

---
+++ test107/Main.py
@@ -0,0 +1,7 @@
+import convert
+
+number = float(input('Type a value: '))
+print(f'{convert.half(number)}')
+print(f'{convert.double(number)}')
+print(f'{convert.percent(number)}')
+print(f'{convert.percentsub(number)}')
---
+++ test107/convert.py
@@ -0,0 +1,11 @@
+def half(n):
+    return n / 2
+
+def double(n):
+    return n * 2
+
+def percent(n):
+    return  n + (n * 10/100)
+
+def percentsub(n):
+    return n - (n *10/100)
